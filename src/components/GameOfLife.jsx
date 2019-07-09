4924626d20321da5a569a1159b9bb5e42a2a6697
Jeffrey Hart <jeffrey.m.hart@avanade.com>
2019-07-09 12:48:53 -0500

    Implemented next cell update on a timer

--- src/components/GameOfLife.jsx
+++ src/components/GameOfLife.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from './Grid';
 import Menu from './Menu';
 import _ from 'lodash';
@@ -8,21 +8,29 @@ export default function GameOfLife() {
   function init() {
     return Array.from(Array(30), () => new Array(30).fill(false));
   }
-
   const [cells, setCells] = useState(init());
+  const [isActive, setIsActive] = useState(false);
 
-  function handleStart() {
-    next();
+  useEffect(() => {
+    if (isActive) {
+      setTimeout(() => {
+        handleNextCells();
+      }, 600);
+    }
+  });
+
+  function toggleStart() {
+    setIsActive(!isActive);
   }
 
-  function next() {
+  function handleNextCells() {
     const game = new GameService(cells);
     const nextCells = game.next();
-
+    console.log('Setting next cells...');
     setCells(nextCells);
   }
 
-  function handleChangeCell(cell) {
+  function toggleCell(cell) {
     const newCells = _.cloneDeep(cells);
     const { rowIndex, columnIndex } = cell.pos;
     newCells[rowIndex][columnIndex] = cell.isActive;
@@ -33,8 +41,8 @@ export default function GameOfLife() {
   return (
     <div>
       <React.Fragment>
-        <Grid cells={cells} handleChangeCell={handleChangeCell} />
-        <Menu handleStart={handleStart} />
+        <Grid cells={cells} toggleCell={toggleCell} />
+        <Menu isActive={isActive} toggleStart={toggleStart} />
       </React.Fragment>
     </div>
   );
